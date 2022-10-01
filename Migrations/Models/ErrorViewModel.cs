abc4135abe64cdac8780bcc112cd0c393f00609b
karimach <45318156+karimach@users.noreply.github.com>
2022-10-01 23:50:15 +0200

    Add files via upload

---
+++ Migrations/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace MvcMovie.Models
+{
+    public class ErrorViewModel
+    {
+
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
